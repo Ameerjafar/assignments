45c9648f889cac5e4fe338f8165119aa6d93954d
Ameer jafar <ameerjafar123@gmail.com>
2024-01-09 15:55:04 +0530

    week-6 several assignments added

--- week-6/1-use-memo/src/App.jsx
+++ week-6/1-use-memo/src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css'
-import { Assignment1 } from './components/Assignment2'
-import { Assignment2 } from './components/Assignment1'
-import Assignment3 from './components/Assignment3'
-
+import { Assignment1 } from './components/Assignment1'
+import { Assignment2 } from './components/Assignment2'
 function App() {
   return (
     <>
-      <Assignment1 />
-      {/* <Assignment2 /> */}
-      {/* <Assignment3 /> */}
+      <Assignment2 />
+      
     </>
   )
 }
